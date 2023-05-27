bb4b7b6e37a7c22da417ffd159e6a87558bc1258
Atharva <51092847+atharvat80@users.noreply.github.com>
2023-05-27 20:05:08 +0100

    added LUKE tokenizer
    added LUKE tokenizer and modified existing tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,15 +1,46 @@
-from typing import Union
-
 import torch
 from transformers import AutoTokenizer
 
 
+class LUKETokenizer():
+    def __init__(self, model_id="studio-ousia/luke-base"):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+    def decode_spans(self, outputs):
+        mentions = []
+        for idx, (positions, mask) in enumerate(zip(outputs.entity_position_ids, 
+                                                    outputs.entity_attention_mask)):
+            mentions.append([
+                self.tokenizer.decode(outputs.input_ids[idx][mention[mention > 0]])
+                for mention in positions[mask == 1].clamp(min=0)
+            ])
+        return mentions
+
+    def __call__(self, 
+                 sentences, 
+                 entity_spans,
+                 return_tensors="pt", 
+                 padding=True, 
+                 truncation=True, 
+                 **kwargs):
+        
+        inputs = self.tokenizer(sentences, 
+                                entity_spans=entity_spans, 
+                                return_tensors=return_tensors, 
+                                padding=padding, 
+                                truncation=truncation, **kwargs)
+        
+        return inputs
+
+
+
 class EnokeeTokenizer():
-    def __init__(self, encoder: str, max_mention_length):
+    def __init__(self, encoder: str, max_mention_length: int):
         self.tokenizer = AutoTokenizer.from_pretrained(encoder, use_fast=True)
         self.max_mention_length = max_mention_length
         
-    def _get_mention_pos_ids(self, offset_mapping: torch.Tensor, 
+    def _get_mention_pos_ids(self, 
+                             offset_mapping: torch.Tensor, 
                              mention_spans: list[tuple[int, int]]) -> torch.Tensor:
         # mask [#mentions, max_mention_length]
         mask = torch.full((len(mention_spans), self.max_mention_length), -1)
@@ -27,20 +58,13 @@ class EnokeeTokenizer():
         mention_tokens = input_ids.index_select(0, pos_ids[pos_ids > 0])
         return self.tokenizer.decode(mention_tokens)
         
-    def __call__(self, inputs: Union[dict, list[dict]]) -> dict:
-        if isinstance(inputs, dict):
-            inputs = [inputs]
-        
-        # get mention spans from offsets and lengths
-        sequences, spans = [], []
-        for inp in inputs:
-            sequences.append(inp['text'])
-            inp_spans = [(s, s+e) for s, e in zip(inp['offsets'], inp['lengths'])]
-            spans.append(inp_spans)
-        
+    def __call__(self, sentences, spans) -> dict:
         # encode inputs and create mask to get mention spans    
-        inputs = self.tokenizer(sequences, return_tensors='pt', padding=True, 
-                                truncation=True, return_offsets_mapping=True)
+        inputs = self.tokenizer(sentences, 
+                                return_tensors='pt', 
+                                padding=True, 
+                                truncation=True, 
+                                return_offsets_mapping=True)
         
         # get positions of tokens corresponding to mentions
         inputs['mention_pos'] = []
